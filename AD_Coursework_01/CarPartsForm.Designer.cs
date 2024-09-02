c051c55fa57c0ae5e50db31a6b8841f32a8e1e40
Sandun Induranga <sandutharu40@gmail.com>
2024-09-02 23:39:54 +0530

    Update UI layout and correct SQL column names
    Changed the locations of various UI elements in CarPartsForm, including DataGridView, TextBoxes, Buttons, and Labels. Adjusted the form size to (929, 610). Corrected SQL commands by renaming the column 'UnitPrice' to 'Price' for both insert and update operations.

--- AD_Coursework_01/CarPartsForm.Designer.cs
+++ AD_Coursework_01/CarPartsForm.Designer.cs
@@ -54,7 +54,7 @@
             this.tblCarParts.AutoGenerateColumns = false;
             this.tblCarParts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.tblCarParts.DataSource = this.abccartradersDataSetBindingSource;
-            this.tblCarParts.Location = new System.Drawing.Point(50, 279);
+            this.tblCarParts.Location = new System.Drawing.Point(90, 279);
             this.tblCarParts.Name = "tblCarParts";
             this.tblCarParts.Size = new System.Drawing.Size(724, 246);
             this.tblCarParts.TabIndex = 0;
@@ -71,28 +71,28 @@
             // 
             // txtId
             // 
-            this.txtId.Location = new System.Drawing.Point(50, 62);
+            this.txtId.Location = new System.Drawing.Point(90, 62);
             this.txtId.Name = "txtId";
             this.txtId.Size = new System.Drawing.Size(199, 20);
             this.txtId.TabIndex = 1;
             // 
             // txtName
             // 
-            this.txtName.Location = new System.Drawing.Point(306, 62);
+            this.txtName.Location = new System.Drawing.Point(346, 62);
             this.txtName.Name = "txtName";
             this.txtName.Size = new System.Drawing.Size(199, 20);
             this.txtName.TabIndex = 1;
             // 
             // txtUnitPrice
             // 
-            this.txtUnitPrice.Location = new System.Drawing.Point(571, 62);
+            this.txtUnitPrice.Location = new System.Drawing.Point(346, 110);
             this.txtUnitPrice.Name = "txtUnitPrice";
             this.txtUnitPrice.Size = new System.Drawing.Size(199, 20);
             this.txtUnitPrice.TabIndex = 1;
             // 
             // txtQtyOnHand
             // 
-            this.txtQtyOnHand.Location = new System.Drawing.Point(50, 110);
+            this.txtQtyOnHand.Location = new System.Drawing.Point(90, 110);
             this.txtQtyOnHand.Name = "txtQtyOnHand";
             this.txtQtyOnHand.Size = new System.Drawing.Size(199, 20);
             this.txtQtyOnHand.TabIndex = 1;
@@ -102,7 +102,7 @@
             this.btnSave.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(157)))), ((int)(((byte)(143)))));
             this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnSave.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.btnSave.Location = new System.Drawing.Point(571, 110);
+            this.btnSave.Location = new System.Drawing.Point(622, 50);
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(193, 32);
             this.btnSave.TabIndex = 2;
@@ -115,7 +115,7 @@
             this.btnUpdate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(233)))), ((int)(((byte)(196)))), ((int)(((byte)(106)))));
             this.btnUpdate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnUpdate.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.btnUpdate.Location = new System.Drawing.Point(571, 152);
+            this.btnUpdate.Location = new System.Drawing.Point(622, 92);
             this.btnUpdate.Name = "btnUpdate";
             this.btnUpdate.Size = new System.Drawing.Size(193, 32);
             this.btnUpdate.TabIndex = 3;
@@ -128,7 +128,7 @@
             this.btnDelete.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(111)))), ((int)(((byte)(81)))));
             this.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnDelete.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.btnDelete.Location = new System.Drawing.Point(571, 197);
+            this.btnDelete.Location = new System.Drawing.Point(622, 137);
             this.btnDelete.Name = "btnDelete";
             this.btnDelete.Size = new System.Drawing.Size(193, 32);
             this.btnDelete.TabIndex = 3;
@@ -141,7 +141,7 @@
             this.btnClear.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(127)))), ((int)(((byte)(0)))));
             this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnClear.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.btnClear.Location = new System.Drawing.Point(571, 235);
+            this.btnClear.Location = new System.Drawing.Point(622, 180);
             this.btnClear.Name = "btnClear";
             this.btnClear.Size = new System.Drawing.Size(193, 32);
             this.btnClear.TabIndex = 3;
@@ -153,7 +153,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.ForeColor = System.Drawing.SystemColors.GrayText;
-            this.label1.Location = new System.Drawing.Point(47, 46);
+            this.label1.Location = new System.Drawing.Point(87, 46);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(59, 13);
             this.label1.TabIndex = 6;
@@ -163,7 +163,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.ForeColor = System.Drawing.SystemColors.GrayText;
-            this.label2.Location = new System.Drawing.Point(303, 46);
+            this.label2.Location = new System.Drawing.Point(343, 46);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(57, 13);
             this.label2.TabIndex = 6;
@@ -173,7 +173,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.ForeColor = System.Drawing.SystemColors.GrayText;
-            this.label3.Location = new System.Drawing.Point(568, 46);
+            this.label3.Location = new System.Drawing.Point(343, 94);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(53, 13);
             this.label3.TabIndex = 6;
@@ -183,7 +183,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.ForeColor = System.Drawing.SystemColors.GrayText;
-            this.label4.Location = new System.Drawing.Point(47, 94);
+            this.label4.Location = new System.Drawing.Point(87, 94);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(92, 13);
             this.label4.TabIndex = 6;
@@ -207,7 +207,7 @@
             this.Controls.Add(this.txtId);
             this.Controls.Add(this.tblCarParts);
             this.Name = "CarPartsForm";
-            this.Size = new System.Drawing.Size(823, 610);
+            this.Size = new System.Drawing.Size(929, 610);
             ((System.ComponentModel.ISupportInitialize)(this.tblCarParts)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.abccartradersDataSetBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.abc_car_tradersDataSet)).EndInit();
